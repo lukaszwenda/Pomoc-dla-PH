2f08ae1b019e429a642799da5e1cc0dedaf6b86d
lukaszwenda <lukaszwenda@yahoo.co.uk>
2025-05-12 19:33:12 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,28 @@
 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open("kalkulator-cache").then(function (cache) {
-      return cache.addAll(["./index.html"]);
-    })
+const CACHE_NAME = 'kp-calculator-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo_kp_128x128.png',
+  '/logo_kp_256x256.png',
+  '/logo_kp_512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
